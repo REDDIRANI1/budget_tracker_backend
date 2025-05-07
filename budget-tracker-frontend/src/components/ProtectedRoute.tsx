b13532feea43d226ee0b04a36432250a9dcffff7
REDDIRANI1 <141508178+REDDIRANI1@users.noreply.github.com>
2025-05-07 11:19:02 +0530

    Delete budget-tracker-frontend directory

--- budget-tracker-frontend/src/components/ProtectedRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-
-const ProtectedRoute = () => {
-  const token = localStorage.getItem('token'); // Ensure you store token as 'accessToken' after login
-  return token ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
